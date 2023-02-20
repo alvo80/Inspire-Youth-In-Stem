433aa6608037268e0c23f628dc7d701387122891
alvo80 <alvinmuriithi12@gmail.com>
2023-02-20 13:30:02 +0300

    my python code

---
+++ Week-1/assignment.py
@@ -0,0 +1,11 @@
+#Write as program to display the a big T pattern
+
+print("     =====================================   ")
+print("                        ||                   ")
+print("                        ||                   ")
+print("                        ||                   ")
+print("                        ||                   ")
+print("                        ||                   ")
+print("                        ||                   ")
+print("                        ||                   ")
+print("                        ||                   ")
